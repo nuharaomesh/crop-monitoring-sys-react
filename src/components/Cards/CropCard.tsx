47323c4e30fa68cd2f2c28436e996b2681bc28be
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-12 23:24:17 +0530

    add prop values to the CropCard

--- src/components/Cards/CropCard.tsx
+++ src/components/Cards/CropCard.tsx
@@ -1,26 +1,26 @@
 import { BsFillPenFill } from "react-icons/bs";
 
-export default function CropCard() {
+export default function CropCard(props) {
     return (
         <div className="crop-card">
             <div className="crop-card-header">
-                <img src="../../../public/download.jpeg" alt="" className="crop-card-img"/>
+                <img src={props.cropImg} alt="" className="crop-card-img"/>
                 <button className="card-update-btn crop-update-btn-pos"><BsFillPenFill color="green" size="18" /></button>
             </div>
             <div className="crop-card-body">
-                <h1 className="card-title">Triticum <span className="card-sub-title">( wheat )</span></h1>
+                <h1 className="card-title">{props.cropName} <span className="card-sub-title">( {props.cropScientificName} )</span></h1>
                 <div>
                     <label className="card-label">category</label>
-                    <h1 className="font-semibold text-green-600 text-20p">Cereal grasses</h1>
+                    <h1 className="font-semibold text-green-600 text-20p">{props.category}</h1>
                 </div>
                 <div className="flex gap-4">
                     <div>
                         <label className="card-label">season</label>
-                        <h1 className="season-title">Rabi season</h1>
+                        <h1 className="season-title">{props.cropSeason}</h1>
                     </div>
                     <div className="">
                         <label className="card-label">time</label>
-                        <h1 className="crop-card-time">100 - 120 days</h1>
+                        <h1 className="crop-card-time">{props.cropGrowthTime}</h1>
                     </div>
                 </div>
             </div>
