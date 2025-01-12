41874e31be7c16b1fa7aebfc1292887ddd0f48ab
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-12 23:25:02 +0530

    get values from the CropForm

--- src/components/Crop/CropForm.tsx
+++ src/components/Crop/CropForm.tsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
-export default function CropForm() {
+export default function CropForm(props) {
 
     const [imagePreview, setImagePreview] = useState<string | null>(null);
 
@@ -9,6 +9,7 @@ export default function CropForm() {
         if (file) {
             const previewUrl = URL.createObjectURL(file);
             setImagePreview(previewUrl);
+            props.setCropImage(previewUrl)
         }
     };
 
@@ -17,7 +18,7 @@ export default function CropForm() {
             <form action="" className="form-border">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h1 className="modal-title">Ad or Update New Crop</h1>
+                        <h1 className="modal-title">{props.title}</h1>
                     </div>
 
                     <div className="modal-body">
@@ -30,22 +31,19 @@ export default function CropForm() {
                         </div>
                         <div>
                             <label htmlFor="crop_name" className="form-label">Crop Name</label>
-                            <input type="text" className="form-control" id="crop_name" placeholder="Enter crop name"/>
+                            <input type="text" className="form-control" id="crop_name" placeholder="Enter crop name" onChange={(e) => props.setCropName(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="crop_scientific_name" className="form-label">Scientific Name</label>
-                            <input type="text" className="form-control" id="crop_scientific_name"
-                                   placeholder="Scientific name"/>
+                            <input type="text" className="form-control" id="crop_scientific_name" placeholder="Scientific name" onChange={(e) => props.setCropScientificName(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="crop_image" className="form-label">Crop Image</label>
-                            <input type="file" className="form-control file-input" id="crop_image" accept="image/*"
-                                   onChange={handleFileChange}/>
+                            <input type="file" className="form-control file-input" id="crop_image" accept="image/*" onChange={handleFileChange}/>
                         </div>
                         <div>
                             <label htmlFor="crop_season" className="form-label">Crop Season</label>
-                            <input className="form-control" list="cropSeasons" placeholder="Select Season"
-                                   id="crop_season"/>
+                            <input className="form-control" list="cropSeasons" placeholder="Select Season" id="crop_season" onChange={(e) => props.setCropSeason(e.target.value)}/>
                             <datalist id="cropSeasons">
                                 <option value="Spring"></option>
                                 <option value="Summer"></option>
@@ -54,8 +52,7 @@ export default function CropForm() {
                         </div>
                         <div>
                             <label htmlFor="crop_category" className="form-label">Crop Category</label>
-                            <input className="form-control" list="cropCategories" placeholder="Select Category"
-                                   id="crop_category"/>
+                            <input className="form-control" list="cropCategories" placeholder="Select Category" id="crop_category" onChange={(e) => props.setCropCategories(e.target.value)}/>
                             <datalist id="cropCategories">
                                 <option value="Fruits"></option>
                                 <option value="Vegetables"></option>
@@ -64,8 +61,7 @@ export default function CropForm() {
                         </div>
                         <div>
                             <label htmlFor="crop_growth_time" className="form-label">Growth Time</label>
-                            <input className="form-control" list="cropGrowthTimes"
-                                   placeholder="Select Growth Time" id="crop_growth_time"/>
+                            <input className="form-control" list="cropGrowthTimes" placeholder="Select Growth Time" id="crop_growth_time" onChange={(e) => props.setCropGrowthTime(e.target.value)}/>
                             <datalist id="cropGrowthTimes">
                                 <option value="Short-term"></option>
                                 <option value="Medium-term"></option>
@@ -74,8 +70,8 @@ export default function CropForm() {
                         </div>
                     </div>
                     <div className="modal-footer">
-                        <button className="cancel-button">Cancel</button>
-                        <button className="save-button">Save</button>
+                        <button className="cancel-button" onClick={props.handleCancel}>Cancel</button>
+                        <button className="save-button" onClick={props.handleSubmit}>{props.children}</button>
                     </div>
                 </div>
             </form>
