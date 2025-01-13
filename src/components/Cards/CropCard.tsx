f0d55dd0b1cbd314a918b604a27fbc9b3d467415
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-13 12:12:58 +0530

    remove button and add Link tag for navigate to the update routes

--- src/components/Cards/CropCard.tsx
+++ src/components/Cards/CropCard.tsx
@@ -1,11 +1,12 @@
 import { BsFillPenFill } from "react-icons/bs";
+import {Link} from "react-router-dom";
 
 export default function CropCard(props) {
     return (
         <div className="crop-card">
             <div className="crop-card-header">
                 <img src={props.cropImg} alt="" className="crop-card-img"/>
-                <button className="card-update-btn crop-update-btn-pos"><BsFillPenFill color="green" size="18" /></button>
+                <Link to={`/crop/updateCrop/${props.cropCode}`} className="card-update-btn crop-update-btn-pos"><BsFillPenFill color="green" size="18" /></Link>
             </div>
             <div className="crop-card-body">
                 <h1 className="card-title">{props.cropName} <span className="card-sub-title">( {props.cropScientificName} )</span></h1>
