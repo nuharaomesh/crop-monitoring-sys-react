30b2ba2fcbc446b5aa9d10c9f6920ea50c66d0b4
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-13 12:13:41 +0530

    set default values if when opening as UpdateForm

--- src/components/Crop/CropForm.tsx
+++ src/components/Crop/CropForm.tsx
@@ -15,7 +15,7 @@ export default function CropForm(props) {
 
     return (
         <div className="modal">
-            <form action="" className="form-border">
+            <form className="form-border">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title">{props.title}</h1>
@@ -26,24 +26,35 @@ export default function CropForm(props) {
                             {imagePreview ? (
                                 <img src={imagePreview} alt="Crop Preview" className="item-image"/>
                             ) : (
-                                <img src="../../../public/images.png" alt="Crop Image" className="item-image"/>
+                                <img src={props.title?.startsWith("Update") ? props.crop.cropImg : "../../../public/images.png"} alt="Crop Image" className="item-image"
+                                />
                             )}
                         </div>
                         <div>
                             <label htmlFor="crop_name" className="form-label">Crop Name</label>
-                            <input type="text" className="form-control" id="crop_name" placeholder="Enter crop name" onChange={(e) => props.setCropName(e.target.value)}/>
+                            <input type="text" className="form-control" id="crop_name"
+                                   placeholder="Enter crop name"
+                                   onChange={(e) => props.setCropName(e.target.value)}
+                                   defaultValue={props.title?.startsWith("Update") ? props.crop.cropName : props.cropName}
+                            />
                         </div>
                         <div>
                             <label htmlFor="crop_scientific_name" className="form-label">Scientific Name</label>
-                            <input type="text" className="form-control" id="crop_scientific_name" placeholder="Scientific name" onChange={(e) => props.setCropScientificName(e.target.value)}/>
+                            <input type="text" className="form-control" id="crop_scientific_name"
+                                   defaultValue={props.title?.startsWith("Update") ? props.crop.cropScientificName : props.cropScientificName}
+                                   placeholder="Scientific name"
+                                   onChange={(e) => props.setCropScientificName(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="crop_image" className="form-label">Crop Image</label>
-                            <input type="file" className="form-control file-input" id="crop_image" accept="image/*" onChange={handleFileChange}/>
+                            <input type="file" className="form-control file-input" id="crop_image" accept="image/*"
+                                   onChange={handleFileChange}/>
                         </div>
                         <div>
                             <label htmlFor="crop_season" className="form-label">Crop Season</label>
-                            <input className="form-control" list="cropSeasons" placeholder="Select Season" id="crop_season" onChange={(e) => props.setCropSeason(e.target.value)}/>
+                            <input className="form-control" list="cropSeasons" placeholder="Select Season" id="crop_season"
+                                   defaultValue={props.title?.startsWith("Update") ? props.crop.cropSeason : props.cropSeason}
+                                   onChange={(e) => props.setCropSeason(e.target.value)}/>
                             <datalist id="cropSeasons">
                                 <option value="Spring"></option>
                                 <option value="Summer"></option>
@@ -52,7 +63,9 @@ export default function CropForm(props) {
                         </div>
                         <div>
                             <label htmlFor="crop_category" className="form-label">Crop Category</label>
-                            <input className="form-control" list="cropCategories" placeholder="Select Category" id="crop_category" onChange={(e) => props.setCropCategories(e.target.value)}/>
+                            <input className="form-control" list="cropCategories" placeholder="Select Category" id="crop_category"
+                                   defaultValue={props.title?.startsWith("Update") ? props.crop.category : props.category}
+                                   onChange={(e) => props.setCropCategories(e.target.value)}/>
                             <datalist id="cropCategories">
                                 <option value="Fruits"></option>
                                 <option value="Vegetables"></option>
@@ -61,7 +74,9 @@ export default function CropForm(props) {
                         </div>
                         <div>
                             <label htmlFor="crop_growth_time" className="form-label">Growth Time</label>
-                            <input className="form-control" list="cropGrowthTimes" placeholder="Select Growth Time" id="crop_growth_time" onChange={(e) => props.setCropGrowthTime(e.target.value)}/>
+                            <input className="form-control" list="cropGrowthTimes" placeholder="Select Growth Time" id="crop_growth_time"
+                                   defaultValue={props.title?.startsWith("Update") ? props.crop.cropGrowthTime : props.cropGrowthTime}
+                                   onChange={(e) => props.setCropGrowthTime(e.target.value)}/>
                             <datalist id="cropGrowthTimes">
                                 <option value="Short-term"></option>
                                 <option value="Medium-term"></option>
@@ -70,8 +85,8 @@ export default function CropForm(props) {
                         </div>
                     </div>
                     <div className="modal-footer">
-                        <button className="cancel-button" onClick={props.handleCancel}>Cancel</button>
-                        <button className="save-button" onClick={props.handleSubmit}>{props.children}</button>
+                        <button type="button" className="cancel-button" onClick={props.handleCancel}>Cancel</button>
+                        <button type="button" className="save-button" onClick={props.handleSubmit}>{props.children}</button>
                     </div>
                 </div>
             </form>
