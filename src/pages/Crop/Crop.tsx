65782cda04422f70af008dafe7b26bbc660d343c
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-14 12:56:23 +0530

    minor changes

--- src/pages/Crop/Crop.tsx
+++ src/pages/Crop/Crop.tsx
@@ -1,11 +1,9 @@
 import CropList from "../../components/Crop/CropList.tsx";
-import CropDetails from "../../components/Crop/CropDetails.tsx";
 
 export default function Crop() {
     return (
         <section className="main-border crop-container">
             <CropList/>
-            <CropDetails/>
         </section>
     )
 }
