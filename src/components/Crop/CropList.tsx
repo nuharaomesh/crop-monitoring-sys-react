3295c22071fae74252c2c83623ad48b43c5194f0
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-14 12:55:49 +0530

    minor changes

--- src/components/Crop/CropList.tsx
+++ src/components/Crop/CropList.tsx
@@ -8,33 +8,38 @@ export default function CropList() {
     const crops = useSelector(state => state.crop)
 
     return (
-        <section className="custom-layout crop-list">
-            <div className="crop-list-header">
-                <div className="list-header-title">
-                    <h1 className="list-title">Manage your crops</h1>
-                    <Link to='/crop/addCrop' className="custom-persist-btn">Add new</Link>
+        <>
+            <section className="custom-layout crop-list">
+                <div className="crop-list-header">
+                    <div className="list-header-title">
+                        <h1 className="list-title">Manage your crops</h1>
+                        <Link to='/crop/addCrop' className="custom-persist-btn">Add new</Link>
+                    </div>
+                    <p className="list-sub-title">search the crop you looking for</p>
                 </div>
-                <p className="list-sub-title">search the crop you looking for</p>
-            </div>
-            <div className="crop-list-body">
-                <div className="crop-searchbar">
-                    <Searchbar/>
+                <div className="crop-list-body">
+                    <div className="crop-searchbar">
+                        <Searchbar/>
+                    </div>
+                    <div className="crop-list-items custom-list-items">
+                        {crops.map(crop => (
+                            <CropCard key={crop.cropCode}
+                                      cropCode={crop.cropCode}
+                                      cropImg={crop.cropImg}
+                                      cropName={crop.cropName}
+                                      cropScientificName={crop.cropScientificName}
+                                      category={crop.category}
+                                      cropSeason={crop.cropSeason}
+                                      cropGrowthTime={crop.cropGrowthTime}
+                            />
+                        ))}
+                    </div>
                 </div>
-                <div className="crop-list-items custom-list-items">
-                    {crops.map(crop => (
-                        <CropCard key={crop.cropCode}
-                                  cropCode={crop.cropCode}
-                                  cropImg={crop.cropImg}
-                                  cropName={crop.cropName}
-                                  cropScientificName={crop.cropScientificName}
-                                  category={crop.category}
-                                  cropSeason={crop.cropSeason}
-                                  cropGrowthTime={crop.cropGrowthTime}
-                        />
-                    ))}
-                </div>
-            </div>
-            <Outlet/>
-        </section>
+                <Outlet/>
+            </section>
+            <section className="custom-layout crop-graphs">
+                <h1>Graps</h1>
+            </section>
+        </>
     )
 }
