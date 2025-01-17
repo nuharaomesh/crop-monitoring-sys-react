4bf964f8ed6c67ce22996941f1c3fe7ab760deee
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-17 16:44:27 +0530

    provide staff slice

---
+++ src/reducers/StaffSlice.ts
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {Staff} from "../models/Staff.ts";
+
+const initialState: Staff[] = []
+const StaffSlice = createSlice({
+    name: 'staff',
+    initialState: initialState,
+    reducers: {
+        add_staff: (state, action) => {
+            state.push(action.payload)
+        },
+        update_staff: (state, action) => {
+            const staff = state.find(staff => staff.staffID === action.payload.staffID)
+            if (staff) Object.assign(staff, { ...action.payload })
+        },
+        delete_staff: (state, action) => {
+            state.filter(staff =>
+                staff.staffID !== action.payload.staffID
+            )
+        }
+    }
+})
+
+export const { add_staff, update_staff, delete_staff } = StaffSlice.actions
+export default StaffSlice.reducer
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../reducers/CropSlice.ts";
 import FieldSlice from "../reducers/FieldSlice.ts";
 import CultivatedSlice from "../reducers/CultivatedSlice.ts";
+import StaffSlice from "../reducers/StaffSlice.ts";
 
 export const store = configureStore({
     reducer: {
         crop: CropSlice,
         field: FieldSlice,
-        cultivate: CultivatedSlice
+        cultivate: CultivatedSlice,
+        staff: StaffSlice,
     }
 })
