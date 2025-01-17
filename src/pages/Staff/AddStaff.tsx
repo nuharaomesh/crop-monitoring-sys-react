5e38dbe6622dcdba66b2fb1cd0322b7172d2f08f
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-17 16:45:17 +0530

    impl staff save page

---
+++ src/pages/Staff/AddStaff.tsx
@@ -0,0 +1,58 @@
+import StaffForm from "../../components/Staff/StaffForm.tsx";
+import React, {useState} from "react";
+import {Staff} from "../../models/Staff.ts";
+import generateID from "../../util/GenerateID.ts";
+import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {add_staff} from "../../reducers/StaffSlice.ts";
+
+export default function AddStaff() {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [gender, setGender] = useState<"male" | "female">("male")
+    const [staffImg, setStaffImg] = useState<string | null>(null)
+    const [dob, setDob] = useState('')
+    const [joinedDate, setJoinedDate] = useState('')
+    const [address, setAddress] = useState('')
+    const [phone, setPhone] = useState(0)
+    const [email, setEmail] = useState('')
+    const [role, setRole] = useState('')
+    const [status, setStatus] = useState<"Active" | "Not Available" | "Available">("Active")
+
+    function handleSubmit(event: React.SyntheticEvent) {
+        event.preventDefault()
+        const genStaffID = generateID("STAFF")
+        const newStaff = new Staff(genStaffID, firstname, lastname, gender, email, role, address, joinedDate, dob, "", String(phone), staffImg, status)
+        dispatch(add_staff({...newStaff}))
+        navigate('/staff')
+    }
+
+    function handleCancel(event: React.SyntheticEvent) {
+        event.preventDefault()
+        navigate('/staff')
+    }
+
+    return (
+        <>
+            <StaffForm
+                title={"Add a new Staff member"}
+                handleSubmit={handleSubmit}
+                handleCancel={handleCancel}
+                setFirstname={setFirstname}
+                setLastname={setLastname}
+                setSelectedFieldType={setGender}
+                setStaffImage={setStaffImg}
+                setDob={setDob}
+                setJoindeDate={setJoinedDate}
+                setAddress={setAddress}
+                setPhoneNo={setPhone}
+                setEmail={setEmail}
+                setStaffRole={setRole}
+            >Save</StaffForm>
+        </>
+    )
+}
