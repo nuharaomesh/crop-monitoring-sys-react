79de4f3c505bcbf49243e6dccd205b24a753d504
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-17 23:42:09 +0530

    style activity layout

---
+++ src/pages/Activity.tsx
@@ -0,0 +1,22 @@
+export default function Activity() {
+    return (
+        <section className="main-border activity-container">
+            <div className="unCultivated-fields custom-layout">
+
+            </div>
+            <div className="activity-details">
+                <div className="activity-more">
+                    <div className="blank-2 custom-layout">
+
+                    </div>
+                    <div className="used-props-details custom-layout">
+
+                    </div>
+                </div>
+                <div className="cultivated-fields custom-layout">
+
+                </div>
+            </div>
+        </section>
+    )
+}
