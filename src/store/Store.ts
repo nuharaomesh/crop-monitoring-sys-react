b81ae286acfefaa53db2f076cb978ee86f663203
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-17 12:23:32 +0530

    add cultivate provider

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../reducers/CropSlice.ts";
 import FieldSlice from "../reducers/FieldSlice.ts";
+import CultivatedSlice from "../reducers/CultivatedSlice.ts";
 
 export const store = configureStore({
     reducer: {
         crop: CropSlice,
-        field: FieldSlice
+        field: FieldSlice,
+        cultivate: CultivatedSlice
     }
 })
