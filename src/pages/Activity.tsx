34cb20a142e389687053e40aa7c223d3e64a00bc
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-31 21:56:24 +0530

    impl charts (dummy data)

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -1,6 +1,47 @@
 import UncultivatedFieldList from "../components/Field/UncultivatedFieldList.tsx";
 import {Outlet} from "react-router-dom";
 
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "../components/ui/chart"
+const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+    { month: "July", desktop: 214, mobile: 140 },
+    { month: "August", desktop: 124, mobile: 240 },
+    { month: "September", desktop: 124, mobile: 240 },
+    { month: "October", desktop: 164, mobile: 240 },
+    { month: "November", desktop: 54, mobile: 240 },
+    { month: "December", desktop: 404, mobile: 240 },
+]
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "#2563eb",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "#60a5fa",
+    },
+} satisfies ChartConfig
+
 export default function Activity() {
     return (
         <section className="main-border activity-container">
@@ -17,7 +58,56 @@ export default function Activity() {
                     </div>
                 </div>
                 <div className="cultivated-fields custom-layout">
-
+                    <div>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Line Chart - Dots</CardTitle>
+                                <CardDescription>January - June 2024</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <ChartContainer config={chartConfig}>
+                                    <LineChart
+                                        accessibilityLayer
+                                        data={chartData}
+                                        margin={{
+                                            left: 12,
+                                            right: 12,
+                                        }}
+                                    >
+                                        <CartesianGrid vertical={false} />
+                                        <XAxis
+                                            dataKey="month"
+                                            tickLine={false}
+                                            axisLine={false}
+                                            tickMargin={8}
+                                            tickFormatter={(value) => value.slice(0, 3)}
+                                        />
+                                        <ChartTooltip
+                                            cursor={false}
+                                            content={<ChartTooltipContent hideLabel />}
+                                        />
+                                        <Line
+                                            dataKey="desktop"
+                                            type="natural"
+                                            stroke="var(--color-desktop)"
+                                            strokeWidth={2}
+                                            dot={{
+                                                fill: "var(--color-desktop)",
+                                            }}
+                                            activeDot={{
+                                                r: 6,
+                                            }}
+                                        />
+                                    </LineChart>
+                                </ChartContainer>
+                            </CardContent>
+                            <CardFooter className="flex-col items-start gap-2 text-sm">
+                                <div className="flex gap-2 font-medium leading-none">
+                                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </div>
                 </div>
             </div>
             <Outlet/>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,163 @@
+import { Calendar } from "../components/ui/calendar"
+import {useState} from "react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "../components/ui/table"
+
+const invoices = [
+    {
+        invoice: "INV001",
+        paymentStatus: "Paid",
+        totalAmount: "$250.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV002",
+        paymentStatus: "Pending",
+        totalAmount: "$150.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV003",
+        paymentStatus: "Unpaid",
+        totalAmount: "$350.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV004",
+        paymentStatus: "Paid",
+        totalAmount: "$450.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV005",
+        paymentStatus: "Paid",
+        totalAmount: "$550.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV006",
+        paymentStatus: "Pending",
+        totalAmount: "$200.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+]
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "../components/ui/chart"
+const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+]
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "#2563eb",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "#60a5fa",
+    },
+} satisfies ChartConfig
+
 export default function Dashboard() {
+    const [date, setDate] = useState<Date | undefined>(new Date())
+
     return (
         <main className="main-border dashboard-container">
             <div className="dashboard-left">
                 <div className="graph-and-progress">
-                    <div className="dashboard-graph custom-layout">
-
+                    <div className="dashboard-graph">
+                        <div>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>Bar Chart - Multiple</CardTitle>
+                                    <CardDescription>January - June 2024</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <ChartContainer config={chartConfig}>
+                                        <BarChart accessibilityLayer data={chartData}>
+                                            <CartesianGrid vertical={false} />
+                                            <XAxis
+                                                dataKey="month"
+                                                tickLine={false}
+                                                tickMargin={10}
+                                                axisLine={false}
+                                                tickFormatter={(value) => value.slice(0, 3)}
+                                            />
+                                            <ChartTooltip
+                                                cursor={false}
+                                                content={<ChartTooltipContent indicator="dashed" />}
+                                            />
+                                            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                                            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                                        </BarChart>
+                                    </ChartContainer>
+                                </CardContent>
+                            </Card>
+                        </div>
                     </div>
-                    <div className="field-progress custom-layout">
-
+                    <div className="custom-calender custom-layout">
+                        <div className="w-fit">
+                            <Calendar
+                                mode="single"
+                                selected={date}
+                                onSelect={setDate}
+                                className="rounded-md"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="dashboard-task custom-layout">
-
+                    <Table>
+                        <TableCaption>A list of your recent invoices.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Invoice</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Method</TableHead>
+                                <TableHead className="text-right">Amount</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {invoices.map((invoice) => (
+                                <TableRow key={invoice.invoice}>
+                                    <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                                    <TableCell>{invoice.paymentStatus}</TableCell>
+                                    <TableCell>{invoice.paymentMethod}</TableCell>
+                                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
             <div className="dashboard-right">
