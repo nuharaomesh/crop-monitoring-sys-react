7e580cc53f7bd70d2de6e6250feaebdba5aa7b67
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-07 00:29:27 +0530

    prevent the refresh in search bar & remove btn

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,11 +1,8 @@
-import {BsSearch} from "react-icons/bs";
-
 export default function Searchbar(props) {
     return (
         <>
-            <form action="" className="custom-searchbar">
+            <form onSubmit={(e) => e.preventDefault()} className="custom-searchbar">
                 <input type="search" placeholder="Search" className="search-input focus:outline-none" onChange={(e) => props.searchValue(e.target.value)}/>
-                <button type="button" className="search-btn"><BsSearch color="gray"/></button>
             </form>
         </>
     )
