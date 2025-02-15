0a86ddcf381a44605aaebdd1398884cce2b09896
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-15 22:35:48 +0530

    minor changes

--- src/components/Staff/StaffList.tsx
+++ src/components/Staff/StaffList.tsx
@@ -1,24 +1,30 @@
-import {Link, Outlet} from "react-router-dom";
+import {Link} from "react-router-dom";
 import Searchbar from "../Searchbar.tsx";
 import StaffCard from "../Cards/StaffCard.tsx";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Staff from "../../models/Staff.ts";
-import {RootState} from "../../store/Store.ts";
+import {AppDispatch, RootState} from "../../store/Store.ts";
 import {useEffect, useState} from "react";
+import { getAllStaffs } from "../../reducers/StaffSlice.ts";
 
 export default function StaffList() {
 
+    const dispatch = useDispatch<AppDispatch>()
     const [searchValue, setSearchValue] = useState("")
-    const staffs: Staff[] = useSelector((state: RootState) => state.staff)
+    const staffs: Staff[] = useSelector(state => state.staff)
     const [filteredStaff, setFilteredStaff] = useState<Staff[]>([])
 
     useEffect(() => {
+        dispatch(getAllStaffs())
+    }, [])
+    
+    useEffect(() => {
         setFilteredStaff(
             staffs.filter((s) =>
-                s.firstname.toLowerCase().includes(searchValue)
+                s.name.toLowerCase().includes(searchValue)
             )
         )
-    }, [searchValue]);
+    }, [searchValue, staffs]);
 
     return (
         <section className="staff-list">
@@ -38,8 +44,8 @@ export default function StaffList() {
                         filteredStaff.map((s) => (
                             <StaffCard key={s.staffID}
                                        staffID={s.staffID}
-                                       img={s.staffImg}
-                                       name={`${s.firstname} ${s.lastname}`}
+                                       img={s.img}
+                                       name={s.name}
                                        role={s.role}
                                        stat={s.status}
                                        phone={s.phone}
@@ -49,8 +55,8 @@ export default function StaffList() {
                     ) : (staffs.map((staff: Staff) => (
                         <StaffCard key={staff.staffID}
                                    staffID={staff.staffID}
-                                   img={staff.staffImg}
-                                   name={`${staff.firstname} ${staff.lastname}`}
+                                   img={staff.img}
+                                   name={staff.name}
                                    role={staff.role}
                                    stat={staff.status}
                                    phone={staff.phone}
