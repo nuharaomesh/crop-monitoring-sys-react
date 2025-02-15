7e8d8cc9e5443de3da2384dd35e1e3730000918d
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-15 22:36:38 +0530

    minor fix

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -16,5 +16,5 @@ export const store = configureStore({
         equipment: EquipmentSlice
     }
 })
-
+export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
