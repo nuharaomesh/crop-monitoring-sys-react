7c3f567168d08a4cd280d922e107430dd9006319
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-22 14:20:11 +0530

    minor changes

--- src/models/Field.ts
+++ src/models/Field.ts
@@ -1,19 +1,19 @@
-export class Field {
+export default class Field {
     fieldCode: string
     fieldName: string
     fieldAddress: string
     fieldLocation: string
     fieldSize: string
-    fieldImg: string | null
-    isCultivated: boolean
+    img: string | null
+    fieldNowCultivated!: boolean
 
-    constructor(fieldCode: string, fieldName: string, fieldAddress: string, fieldLocation: string, fieldSize: string, fieldImg: string | null, isCultivated: boolean) {
+    constructor(fieldCode: string, fieldName: string, fieldAddress: string, fieldLocation: string, fieldSize: string, img: string | null, fieldNowCultivated: boolean) {
         this.fieldCode = fieldCode
         this.fieldName = fieldName
         this.fieldAddress = fieldAddress
         this.fieldLocation = fieldLocation
         this.fieldSize = fieldSize
-        this.fieldImg = fieldImg
-        this.isCultivated = isCultivated
+        this.img = img
+        this.fieldNowCultivated = fieldNowCultivated
     }
 }
